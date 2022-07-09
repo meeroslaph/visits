99e3ee1c4cb27a5c9ab9c77798708d017b491a20
Myroslav Oleinykov <m@meeroslaph.name>
2022-07-09 18:07:09 +0200

    App server starter

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import redis from 'redis';
+
+const app = express();
+const client = redis.createClient();
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Server is running on port 8081');
+});
